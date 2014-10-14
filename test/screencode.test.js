dcafb5a2d8f62a54c506d06b4199d37008e780bc
Nick Kaye <nick.c.kaye@gmail.com>
2014-10-14 12:52:23 -0400

    Basic CLI

--- test/screencode.test.js
+++ test/screencode.test.js
@@ -1,3 +1,14 @@
+/**
+ * Screencode™
+ *
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ * @laboratory Outright Mental Inc.
+ * @code https://github.com/outrightmental/screencode
+ *
+ * Copyright (c) 2014 Outright Mental Inc.
+ * Licensed under the MIT license.
+ */
+
 var should = require('chai').should(),
     screencode = require('../lib/screencode');
 
