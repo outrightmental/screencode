134c285512c114bd0e6448b0d842fb59711c04b6
Nick Kaye <nick.c.kaye@gmail.com>
2014-10-14 18:06:44 -0400

    Basic Git interaction.

---
+++ test/service/repository.service.test.js
@@ -0,0 +1,17 @@
+/**
+ * Screencode™
+ *
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ * @laboratory Outright Mental Inc.
+ * @code https://github.com/outrightmental/screencode
+ *
+ * Copyright (c) 2014 Outright Mental Inc.
+ * Licensed under the MIT license.
+ */
+
+var should = require('chai').should(),
+  repo = require('../../lib/service/repository.service.js');
+
+// TODO: Write tests
+
+// Initial state: Loading
